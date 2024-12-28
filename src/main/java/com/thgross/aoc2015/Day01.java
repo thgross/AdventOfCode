cf2e6c515fffbc71e7d280a4c0fd80a4b92d436f
thgross <thomasrgross@gmx.de>
2024-12-28 01:43:04 +0100

    AOC 2015: Day 01 completed

--- src/main/java/com/thgross/aoc2015/Day01.java
+++ src/main/java/com/thgross/aoc2015/Day01.java
@@ -1,12 +1,8 @@
 package com.thgross.aoc2015;
 
 import com.thgross.aoc.Application;
-import com.thgross.aoc2024.day25.Heightmap;
-import com.thgross.aoc2024.day25.Key;
-import com.thgross.aoc2024.day25.Lock;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Day01 extends Application {
@@ -22,15 +18,12 @@ public class Day01 extends Application {
     @Override
     protected void calcAll(List<String> lines) throws IOException {
 
-        List<Character> chars = new ArrayList<>();
-
         int basementSteps = 0;
         int currentFloor = 0;
 
         for (String line : lines) {
             for (int i = 0; i < line.length(); i++) {
                 var chr = line.charAt(i);
-                chars.add(chr);
                 if (chr == '(') {
                     currentFloor++;
                 } else {
