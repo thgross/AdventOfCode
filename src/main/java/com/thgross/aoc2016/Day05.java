781ace47084b62a5d77035397d4ebe8d71ec6bdf
thgross <thomasrgross@gmx.de>
2025-10-05 15:04:55 +0200

    AOC2016: Day05 Part 2 completed

--- src/main/java/com/thgross/aoc2016/Day05.java
+++ src/main/java/com/thgross/aoc2016/Day05.java
@@ -22,6 +22,7 @@ public class Day05 extends Application {
 
         String doorIdBase = lines.getFirst();
         StringBuilder p1Password = new StringBuilder();
+        char[] p2Password = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
         MessageDigest md;
         try {
             md = MessageDigest.getInstance("MD5");
@@ -29,12 +30,14 @@ public class Day05 extends Application {
             throw new RuntimeException(e);
         }
 
-        int pwCharCount = 0;
+        int pw1CharCount = 0;
+        int pw2CharCount = 0;
         long index = 0;
         byte[] doorId;
         byte[] doorIdMD5;
         String doorIdMD5Hex;
-        while (pwCharCount < 8) {
+        int pw2Charpos;
+        while (pw1CharCount < 8 || pw2CharCount < 8) {
             doorId = (doorIdBase + index).getBytes(StandardCharsets.US_ASCII);
             doorIdMD5 = md.digest(doorId);
             doorIdMD5Hex = HexFormat.of().formatHex(doorIdMD5);
@@ -51,13 +54,37 @@ public class Day05 extends Application {
             index++;
 
             if (doorIdMD5Hex.startsWith("00000")) {
-                p1Password.append(doorIdMD5Hex.charAt(5));
-                pwCharCount++;
+                if (pw1CharCount < 8) {
+                    p1Password.append(doorIdMD5Hex.charAt(5));
+                    pw1CharCount++;
+                }
+
+                if (pw2CharCount < 8) {
+                    pw2Charpos = doorIdMD5Hex.charAt(5) - '0';
+                    if (pw2Charpos >= 0 && pw2Charpos <= 7) {
+                        if (p2Password[pw2Charpos] == ' ') {
+                            p2Password[pw2Charpos] = doorIdMD5Hex.charAt(6);
+                            pw2CharCount++;
+
+                            System.out.printf(
+                                    "Index: %8d | pw2CharCount: %d | pw2Charpos: %d | doorId: %12s | p1Password: %s | doorIdMD5Hex: %s\n",
+                                    index,
+                                    pw2CharCount,
+                                    pw2Charpos,
+                                    new String(doorId, StandardCharsets.US_ASCII),
+                                    String.valueOf(p2Password),
+                                    doorIdMD5Hex
+                            );
+                        }
+                    }
+                }
             }
         }
 
-
         System.out.printf("Part 1 Password: %s\n", p1Password);
-//        System.out.printf("Part 1 Sector ID: %d\n", idLocationPart2);
+        System.out.printf("Part 2 Password: %s\n", String.valueOf(p2Password));
+
+        int test = '9' - '0';
+        System.out.printf("%d\n", test);
     }
 }
