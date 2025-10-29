c5abfff4610e3b5420a692aa644d7c189800ab9d
thgross <thomasrgross@gmx.de>
2025-10-30 00:06:33 +0100

    AOC2016: Day09 Part 2 completed

--- src/main/java/com/thgross/aoc2016/Day09.java
+++ src/main/java/com/thgross/aoc2016/Day09.java
@@ -11,6 +11,9 @@ public class Day09 extends Application {
 
     String inputFilename = "aoc2016/input09.txt";
 
+    Pattern patternMarker = Pattern.compile("\\((\\d+)x(\\d+)\\)");
+    Matcher matcher;
+
     public static void main(String[] args) {
         var app = (new Day09());
         app.run(app.inputFilename);
@@ -20,14 +23,12 @@ public class Day09 extends Application {
 
         StringBuilder sbPart1 = new StringBuilder();
         int part1Length;
+        long part2Length;
 
         var line = lines.getFirst();
         int lpos = 0;
 
-        var patternMarker = Pattern.compile("\\((\\d+)x(\\d+)\\)");
-        Matcher matcher;
-
-        int markerLength, markerPosition, charCount, repetitions;
+        int markerLength, markerPosition, markerCharcount, markerRepetitions;
         String part;
 
         do {
@@ -35,28 +36,62 @@ public class Day09 extends Application {
             if (matcher.find(lpos)) {
                 markerLength = matcher.group().length();
                 markerPosition = matcher.start();
-                charCount = Integer.parseInt(matcher.group(1));
-                repetitions = Integer.parseInt(matcher.group(2));
-                if(markerPosition > lpos) {
+                markerCharcount = Integer.parseInt(matcher.group(1));
+                markerRepetitions = Integer.parseInt(matcher.group(2));
+                if (markerPosition > lpos) {
+                    // Vor dem Marker sind noch Zeichen
                     sbPart1.append(line, lpos, markerPosition);
                 }
                 lpos = markerPosition + markerLength;
-                part = line.substring(lpos, lpos + charCount);
-                sbPart1.append(part.repeat(repetitions));
-                lpos += charCount;
+                part = line.substring(lpos, lpos + markerCharcount);
+                // Subpart wiederholen
+                sbPart1.append(part.repeat(markerRepetitions));
+                lpos += markerCharcount;
 //                System.out.printf("%s [%d]: '%s'\n", matcher.group(0), markerLength, part);
             } else {
-                if(lpos < line.length()) {
+                if (lpos < line.length()) {
+                    // Nach dem letzten Marker sind noch Zeichen übrig
                     sbPart1.append(line, lpos, line.length());
                 }
                 break;
             }
         } while (true);
 
-//        System.out.println(sbPart1);
         part1Length = sbPart1.length();
 
+        // Part 2
+        part2Length = calcSequenceSize(line, 0, line.length());
+
         System.out.printf("Part 1 Length: %d\n", part1Length);
-//        System.out.printf("Part 2 SSL support: %s\n", sslSupport);
+        System.out.printf("Part 2 Length: %d\n", part2Length);
+    }
+
+    long calcSequenceSize(String line, int start, int end) {
+        long count = 0;
+        int markerLength, markerPosition, markerCharcount, markerRepetitions;
+        do {
+            matcher = patternMarker.matcher(line);
+            if (matcher.find(start) && matcher.start() < end) {
+                markerLength = matcher.group().length();
+                markerPosition = matcher.start();
+                markerCharcount = Integer.parseInt(matcher.group(1));
+                markerRepetitions = Integer.parseInt(matcher.group(2));
+                if (markerPosition > start) {
+                    // Vor dem Marker sind noch Zeichen
+                    count += markerPosition - start;
+                }
+                start = markerPosition + markerLength;
+                count += markerRepetitions * calcSequenceSize(line, start, start + markerCharcount);
+                start += markerCharcount;
+            } else {
+                if (start < end) {
+                    // Nach dem letzten Marker sind noch Zeichen übrig
+                    count += end - start;
+                }
+                break;
+            }
+        } while (true);
+
+        return count;
     }
 }
