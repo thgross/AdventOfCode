a50b0dcd5c29942a3a8440f914e97c300033aa89
thgross <thomasrgross@gmx.de>
2026-01-17 18:31:52 +0100

    AOC2025: Day 2 completed

--- src/main/java/com/thgross/aoc2025/Day02.java
+++ src/main/java/com/thgross/aoc2025/Day02.java
@@ -3,6 +3,7 @@ package com.thgross.aoc2025;
 import com.thgross.aoc.Application;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day02 extends Application {
@@ -18,10 +19,12 @@ public class Day02 extends Application {
     protected void calcAll(List<String> lines) throws IOException {
 
         long part1InvalidIdSum = 0;
+        long part2InvalidIdSum = 0;
 
         long from, to, range, rangeNr;
-        int evenDigits;
-        long digitDiv;
+        int anzahlStellen;
+        List<Integer> digitsTeiler;
+        long digitDivisor;
 
         var ranges = lines.getFirst().split(",");
         rangeNr = 0;
@@ -33,17 +36,53 @@ public class Day02 extends Application {
 
 //            System.out.printf("Range %3d: %d to %d (%d) | ", rangeNr, from, to, range);
 
-            if (getEvenDigits(from) > 0 || getEvenDigits(to) > 0) {
-                for (long i = from; i <= to; i++) {
-                    evenDigits = getEvenDigits(i);
-                    if (evenDigits > 0) {
-                        digitDiv = getDigitDiv(evenDigits / 2);
-                        if (i / digitDiv == i % digitDiv) {
+            for (long i = from; i <= to; i++) {
+
+                anzahlStellen = getAnzahlStellen(i);
+                digitsTeiler = teilerGroesser1(anzahlStellen);
+                if (!digitsTeiler.isEmpty()) {
+
+                    // Damit keine ID doppelt gezählt wird (z.B. bei 222222: 6*2, 3*22, 2*222)
+                    long lastInvalidId = 0;
+
+                    // Part 2
+                    for (Integer teiler : digitsTeiler) {
+                        long itmp = i;
+                        digitDivisor = getDigitDivisor(anzahlStellen / teiler);
+                        long firstPart = i % digitDivisor;
+                        if(firstPart == 0) {continue;}
+                        boolean partsSame = true;
+                        for (int part = 1; part < teiler; part++) {
+                            itmp = itmp / digitDivisor;
+                            if (itmp % digitDivisor != firstPart) {
+                                partsSame = false;
+                                break;
+                            }
+                        }
+                        if(partsSame && lastInvalidId == 0) {
+                            lastInvalidId = i;
+                            part2InvalidIdSum += i;
+//                            System.out.printf("%s -> %d: (%d * %d)\n", rangeString, i, teiler, firstPart);
+                        }
+                    }
+                    // Part 1
+                    if (digitsTeiler.getFirst() == 2) {
+                        digitDivisor = getDigitDivisor(anzahlStellen / 2);
+                        if (i / digitDivisor == i % digitDivisor) {
                             // Hier haben wir eine invalid ID
                             part1InvalidIdSum += i;
                         }
                     }
+
                 }
+
+                // Part 2
+                // Wir müssen zuerst alle Teiler der Stellenzahl ermitteln.
+//                var alleTeiler = teiler(i);
+//                for (Long t : alleTeiler) {
+
+//                }
+                // TODO: implementieren
             }
 
             rangeNr++;
@@ -51,20 +90,42 @@ public class Day02 extends Application {
 
         System.out.println("------------------------------------");
         System.out.printf("Part 1 invalid ID Sum: %d\n", part1InvalidIdSum);
+        System.out.printf("Part 2 invalid ID Sum: %d\n", part2InvalidIdSum);
     }
 
-    int getEvenDigits(long num) {
+    // ermittelt die Anzahl Stellen, wenn diese gerade ist. Bei ungerader Stellenanzahl wird 0 zurückgegeben.
+    int getAnzahlStellen(long num) {
+        if (num >= 0L) if (num < 10L) return 1;
         if (num >= 10L) if (num < 100L) return 2;
+        if (num >= 100L) if (num < 1000L) return 3;
         if (num >= 1000L) if (num < 10000L) return 4;
+        if (num >= 10000L) if (num < 100000L) return 5;
         if (num >= 100000L) if (num < 1000000L) return 6;
+        if (num >= 1000000L) if (num < 10000000L) return 7;
         if (num >= 10000000L) if (num < 100000000L) return 8;
+        if (num >= 100000000L) if (num < 1000000000L) return 9;
         if (num >= 1000000000L) if (num < 10000000000L) return 10;
+        if (num >= 10000000000L) if (num < 100000000000L) return 11;
         if (num >= 100000000000L) if (num < 1000000000000L) return 12;
+        if (num >= 1000000000000L) if (num < 10000000000000L) return 13;
         if (num >= 10000000000000L) if (num < 100000000000000L) return 14;
         return 0;
     }
 
-    long getDigitDiv(int digits) {
+    List<Integer> teilerGroesser1(int zahl) {
+        List<Integer> teiler = new ArrayList<>();
+        for (int i = 2; i <= zahl; i++) {
+            if (zahl % i == 0) {
+                teiler.add(i);
+            }
+        }
+        return teiler;
+    }
+
+    // Gibt den Divisor zurück für eine bestimmte Anzah Stellen. Damit kann man zum Beispiel den oberen und den unteren
+    // Teil einer 6-stelligen Zahl ermitteln, wenn man als `digits` 3 übergibt. Die Methode gibt bei einem Parameter von
+    // 3 eine 1000 zurück, welche für Integer-Division bzw. Modulo verwendet werden kann.
+    long getDigitDivisor(int digits) {
 // Array speichert 10^0 bis 10^18. Der Index entspricht dem Exponenten.
         // Index i speichert 10^i. Wir benötigen 10^(digits).
         final long[] powerOf10 = {
